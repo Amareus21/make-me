ac353ec1385fc4e959739e501b9959939777399a
Miguel <miguelortiz8105@gmail.com>
2024-01-05 14:56:12 +0100

    Add juego

--- app/juegos/data.js
+++ app/juegos/data.js
@@ -7,5 +7,10 @@ export const dataJuegos = [
     texto:
       "Pollito azul: \nW para moverte hacia delante. A para moverte hacia la izquierda. S para moverte hacia a bajo. D para moverte hacia la derecha. E para disparar.\nPollito rojo: \nLas flechs y el espacio para disparar",
   },
-
+  { key: "2",
+  tituloPrincipal : "Spotify memes",
+  src: "https://scratch.mit.edu/projects/945786936/embed",
+  titulo: "Instrucciones",
+  texto:
+  "La manzana es un istereg. El tick es para que nos dejes tu opinion. Y la cruz es para lo mismo que el tick pero en negativo y te cambia la pagina."}
 ];
