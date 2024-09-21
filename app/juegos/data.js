03fdd63a33e69a4d41bb395d8baa2ac455f478c2
SuriK13 <amaroksurinach@gmail.com>
2024-09-21 20:15:42 +0200

    Añadir juegos

--- app/juegos/data.js
+++ app/juegos/data.js
@@ -1,3 +1,4 @@
+import src from "tailwindcss-animated";
 import Parrafo from "../components/parrafo";
 
 export const dataJuegos = [
@@ -5,9 +6,58 @@ export const dataJuegos = [
     key: "1",
     tituloPrincipal: "Pio Pio",
     src: "https://scratch.mit.edu/projects/922045006/embed",
-    titulo: "Instrucciones",
+    titulo: "Instrucciones Y Notas",
     texto:
-      "Pollito azul: \nW para moverte hacia delante. A para moverte hacia la izquierda. S para moverte hacia a bajo. D para moverte hacia la derecha. E para disparar.\nPollito rojo: \nLas flechs y el espacio para disparar",
+      "INSTRUCCIONES:Pollito azul: W para moverte hacia delante. A para moverte hacia la izquierda. S para moverte hacia a bajo. D para moverte hacia la derecha. E para disparar.\nPollito rojo:Las flechs y el espacio para disparar. \nNOTAS: Este juego fue el primer juego que creamos y le tengo mucho aprecio.",
+  },
+  {
+    key: "2",
+    tituloPrincipal: "Spotify MEMES",
+    src: "https://scratch.mit.edu/projects/945786936/embed",
+    titulo: "Instrucciones",
+    texto: "pulsa las letras de debajo para que suene el meme."
+  },
+  {
+    key: "3",
+    tituloPrincipal: "Busca La Teva Llengua",
+    src:"https://scratch.mit.edu/projects/1013183487/embed",
+    titulo: "Instrucciones Y Notas",
+    texto: "INSTRUCCOINES: w,a,s,d para moverte. Para destruir los meteoritos te tienes que hacercar a ellos y presionar la tecla espacio. \nNOTAS: este juego es de una competicion de Scratch claramente echo por nosotros no logramos ganar pero no pasa nada."
+  },
+  {
+    key: "4",
+    tituloPrincipal: "Adivina el Número",
+    src: "https://scratch.mit.edu/projects/964013063/embed",
+    titulo: "Instrucciones Y Notas",
+    texto: "INSTRUCCIONES: En la barra que te sale tienes que poner el numero que tu creas que es y despues le tienesque dar en Adivinar para comprobar que sea el numero el correcto. \nNOTAS: dale al corazon para que cree mas juegos de este tipo."
+  },
+  {
+    key: "5",
+    tituloPrincipal: "Seagull Shooter",
+    src: "https://scratch.mit.edu/projects/1066948195/embed",
+    titulo: "Instrucciones Y Notas",
+    texto: "INSTRUCCIONES: dale click a la gaviota para m4t4rla. \nNOTAS:Este juego no lo tomeis como referencia de la vida real."
+  },
+  {
+    key: "6",
+    tituloPrincipal: "Flappy Bird",
+    src: "https://scratch.mit.edu/projects/1054879356/embed",
+    titulo:"Instrucciones Y Notas",
+    texto: "INSTRUCCIONES: Tecla espacio para saltar. \nNOTAS: Este juego es un record personal. Este juego esta echo en 21minutos"
+  },
+  {
+    key: "7",
+    tituloPrincipal: "THE HOLL (PROXIMAMENTE...)",
+    src: "https://scratch.mit.edu/projects/1070197188/embed",
+    titulo: "Fecha de Realizacion",
+    texto: "Este juego se realizara el Abril del 2025!!"
+  },
+  {
+    key: "8",
+    tituloPrincipal: "Cat-app (PROXIMAMENTE...)",
+    src: "",
+    titulo: "Fecha de Realizacion",
+    texto: "Este juego se realizara el Abril del 2025!!"
   },
 
 ];
